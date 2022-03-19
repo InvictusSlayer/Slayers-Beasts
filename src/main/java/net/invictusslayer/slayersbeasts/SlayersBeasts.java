ac2dc00692f3b3101897a9cca5214842fc9a5288
InvictusSlayer <ojsutters@gmail.com>
2022-03-19 17:47:19 +0000

    Initial commit

---
+++ src/main/java/net/invictusslayer/slayersbeasts/SlayersBeasts.java
@@ -0,0 +1,35 @@
+package net.invictusslayer.slayersbeasts;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(SlayersBeasts.MOD_ID)
+public class SlayersBeasts
+{
+    public static final String MOD_ID = "slayersbeasts";
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public SlayersBeasts()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        IEventBus eventBus = MinecraftForge.EVENT_BUS;
+        eventBus.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
