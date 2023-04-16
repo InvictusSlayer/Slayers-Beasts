0be3fa152ffd694c342b998018976adce63f08c2
InvictusSlayer <ojsutters@gmail.com>
2023-04-16 17:56:43 +0100

    added vanilla blocktags

--- src/main/java/net/invictusslayer/slayersbeasts/datagen/ModBlockTagsProvider.java
+++ src/main/java/net/invictusslayer/slayersbeasts/datagen/ModBlockTagsProvider.java
@@ -6,7 +6,6 @@ import net.invictusslayer.slayersbeasts.util.ModTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
-import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -26,7 +25,25 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
         this.tag(ModTags.Blocks.EUCALYPTUS_LOGS).add(ModBlocks.EUCALYPTUS_LOG.get()).add(ModBlocks.STRIPPED_EUCALYPTUS_LOG.get()).add(ModBlocks.EUCALYPTUS_WOOD.get()).add(ModBlocks.STRIPPED_EUCALYPTUS_WOOD.get());
 
         this.tag(BlockTags.SAND).add(ModBlocks.BLACK_SAND.get());
-        this.tag(BlockTags.LOGS).addTag(ModTags.Blocks.CAJOLE_LOGS).addTag(ModTags.Blocks.EUCALYPTUS_LOGS);
+        this.tag(BlockTags.DIRT).add(ModBlocks.ANT_SOIL.get());
+        this.tag(BlockTags.LEAVES).add(ModBlocks.CAJOLE_LEAVES.get()).add(ModBlocks.EUCALYPTUS_LEAVES.get());
         this.tag(BlockTags.LOGS_THAT_BURN).addTag(ModTags.Blocks.CAJOLE_LOGS).addTag(ModTags.Blocks.EUCALYPTUS_LOGS);
+        this.tag(BlockTags.PLANKS).add(ModBlocks.CAJOLE_PLANKS.get()).add(ModBlocks.EUCALYPTUS_PLANKS.get());
+        this.tag(BlockTags.WOODEN_SLABS).add(ModBlocks.CAJOLE_SLAB.get()).add(ModBlocks.EUCALYPTUS_SLAB.get());
+        this.tag(BlockTags.WOODEN_STAIRS).add(ModBlocks.CAJOLE_STAIRS.get()).add(ModBlocks.EUCALYPTUS_STAIRS.get());
+        this.tag(BlockTags.WOODEN_FENCES).add(ModBlocks.CAJOLE_FENCE.get()).add(ModBlocks.EUCALYPTUS_FENCE.get());
+        this.tag(BlockTags.WOODEN_BUTTONS).add(ModBlocks.CAJOLE_BUTTON.get()).add(ModBlocks.EUCALYPTUS_BUTTON.get());
+        this.tag(BlockTags.WOODEN_PRESSURE_PLATES).add(ModBlocks.CAJOLE_PRESSURE_PLATE.get()).add(ModBlocks.EUCALYPTUS_PRESSURE_PLATE.get());
+        this.tag(BlockTags.WOODEN_DOORS).add(ModBlocks.CAJOLE_DOOR.get()).add(ModBlocks.EUCALYPTUS_DOOR.get());
+        this.tag(BlockTags.WOODEN_TRAPDOORS).add(ModBlocks.CAJOLE_TRAPDOOR.get()).add(ModBlocks.EUCALYPTUS_TRAPDOOR.get());
+        this.tag(BlockTags.FENCE_GATES).add(ModBlocks.CAJOLE_FENCE_GATE.get()).add(ModBlocks.EUCALYPTUS_FENCE_GATE.get());
+        this.tag(BlockTags.SAPLINGS).add(ModBlocks.CAJOLE_SAPLING.get()).add(ModBlocks.EUCALYPTUS_SAPLING.get());
+        this.tag(BlockTags.MUSHROOM_GROW_BLOCK).add(ModBlocks.ANT_SOIL.get());
+        this.tag(BlockTags.MINEABLE_WITH_AXE).add(ModBlocks.WHITE_MUSHROOM_BLOCK.get(), ModBlocks.WHITE_MUSHROOM.get());
+        this.tag(BlockTags.MINEABLE_WITH_HOE).add(ModBlocks.CAJOLE_LEAVES.get(), ModBlocks.EUCALYPTUS_LEAVES.get(), ModBlocks.OOTHECA.get());
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.JADE_BLOCK.get(), ModBlocks.EXOSKELETON_ORE.get(), ModBlocks.DEEPSLATE_EXOSKELETON_ORE.get());
+        this.tag(BlockTags.MINEABLE_WITH_SHOVEL).add(ModBlocks.BLACK_SAND.get(), ModBlocks.ANT_SOIL.get()).addTag(ModTags.Blocks.ANTHILLS);
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL).add(ModBlocks.EXOSKELETON_ORE.get(), ModBlocks.DEEPSLATE_EXOSKELETON_ORE.get());
+        this.tag(BlockTags.NEEDS_IRON_TOOL).add(ModBlocks.JADE_BLOCK.get());
     }
 }
