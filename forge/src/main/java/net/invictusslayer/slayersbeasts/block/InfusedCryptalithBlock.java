2fff54ae4402dc26c40fe166efdeb99e20ffeaea
InvictusSlayer <ojsutters@gmail.com>
2024-03-07 10:52:25 +0000

    v3.0.5 cleaned up infused cryptalith class for version consistency

--- forge/src/main/java/net/invictusslayer/slayersbeasts/block/InfusedCryptalithBlock.java
+++ forge/src/main/java/net/invictusslayer/slayersbeasts/block/InfusedCryptalithBlock.java
@@ -24,50 +24,50 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class InfusedCryptalithBlock extends Block {
-    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
-    public static final BooleanProperty ACTIVE = BlockStateProperties.EYE;
-    private static BlockPattern portalShape;
+	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+	public static final BooleanProperty ACTIVE = BlockStateProperties.EYE;
+	private static BlockPattern portalShape;
+	
+	public InfusedCryptalithBlock(Properties properties) {
+		super(properties);
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(ACTIVE, false));
+	}
 
-    public InfusedCryptalithBlock(Properties pProperties) {
-        super(pProperties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(ACTIVE, false));
-    }
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(FACING, ACTIVE);
+	}
 
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(FACING, ACTIVE);
-    }
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite()).setValue(ACTIVE, false);
+	}
 
-    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        return defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite()).setValue(ACTIVE, false);
-    }
+	public static BlockPattern getOrCreatePortalShape() {
+		if (portalShape == null) {
+			portalShape = BlockPatternBuilder.start().aisle("?vv?", ">??<", ">??<", "?^^?")
+					.where('?', BlockInWorld.hasState(BlockStatePredicate.ANY))
+					.where('^', BlockInWorld.hasState(BlockStatePredicate.forBlock(SBBlocks.INFUSED_CRYPTALITH.get())
+							.where(ACTIVE, Predicates.equalTo(true)).where(FACING, Predicates.equalTo(Direction.SOUTH))))
+					.where('>', BlockInWorld.hasState(BlockStatePredicate.forBlock(SBBlocks.INFUSED_CRYPTALITH.get())
+							.where(ACTIVE, Predicates.equalTo(true)).where(FACING, Predicates.equalTo(Direction.WEST))))
+					.where('v', BlockInWorld.hasState(BlockStatePredicate.forBlock(SBBlocks.INFUSED_CRYPTALITH.get())
+							.where(ACTIVE, Predicates.equalTo(true)).where(FACING, Predicates.equalTo(Direction.NORTH))))
+					.where('<', BlockInWorld.hasState(BlockStatePredicate.forBlock(SBBlocks.INFUSED_CRYPTALITH.get())
+							.where(ACTIVE, Predicates.equalTo(true)).where(FACING, Predicates.equalTo(Direction.EAST)))).build();
+		}
 
-    public static BlockPattern getOrCreatePortalShape() {
-        if (portalShape == null) {
-            portalShape = BlockPatternBuilder.start().aisle("?vv?", ">??<", ">??<", "?^^?")
-                    .where('?', BlockInWorld.hasState(BlockStatePredicate.ANY))
-                    .where('^', BlockInWorld.hasState(BlockStatePredicate.forBlock(SBBlocks.INFUSED_CRYPTALITH.get())
-                            .where(ACTIVE, Predicates.equalTo(true)).where(FACING, Predicates.equalTo(Direction.SOUTH))))
-                    .where('>', BlockInWorld.hasState(BlockStatePredicate.forBlock(SBBlocks.INFUSED_CRYPTALITH.get())
-                            .where(ACTIVE, Predicates.equalTo(true)).where(FACING, Predicates.equalTo(Direction.WEST))))
-                    .where('v', BlockInWorld.hasState(BlockStatePredicate.forBlock(SBBlocks.INFUSED_CRYPTALITH.get())
-                            .where(ACTIVE, Predicates.equalTo(true)).where(FACING, Predicates.equalTo(Direction.NORTH))))
-                    .where('<', BlockInWorld.hasState(BlockStatePredicate.forBlock(SBBlocks.INFUSED_CRYPTALITH.get())
-                            .where(ACTIVE, Predicates.equalTo(true)).where(FACING, Predicates.equalTo(Direction.EAST)))).build();
-        }
+		return portalShape;
+	}
 
-        return portalShape;
-    }
+	@SuppressWarnings("deprecation")
+	public boolean isPathfindable(BlockState state, BlockGetter level, BlockPos pos, PathComputationType type) {
+		return false;
+	}
 
-    @SuppressWarnings("deprecation")
-    public boolean isPathfindable(BlockState pState, BlockGetter pLevel, BlockPos pPos, PathComputationType pType) {
-        return false;
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
-        double d0 = pos.getX() + random.nextDouble();
-        double d1 = pos.getY() + 0.8D;
-        double d2 = pos.getZ() + random.nextDouble();
-        level.addParticle(ParticleTypes.WHITE_ASH, d0, d1, d2, 0.0D, 0.0D, 0.0D);
-    }
+	@OnlyIn(Dist.CLIENT)
+	public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+		double d0 = pos.getX() + random.nextDouble();
+		double d1 = pos.getY() + 0.8D;
+		double d2 = pos.getZ() + random.nextDouble();
+		level.addParticle(ParticleTypes.WHITE_ASH, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+	}
 }
