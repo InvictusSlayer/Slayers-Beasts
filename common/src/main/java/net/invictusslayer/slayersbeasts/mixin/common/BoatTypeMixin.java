ebd81727c5ca1bc40a299e76a96cc45a5c116d31
InvictusSlayer <ojsutters@gmail.com>
2025-08-24 19:31:34 +0100

    Update scabbard version

--- common/src/main/java/net/invictusslayer/slayersbeasts/mixin/common/BoatTypeMixin.java
+++ common/src/main/java/net/invictusslayer/slayersbeasts/mixin/common/BoatTypeMixin.java
@@ -5,6 +5,7 @@ import net.invictusslayer.slayersbeasts.init.SBBlocks;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
