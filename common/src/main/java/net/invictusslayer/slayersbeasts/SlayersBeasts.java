20329c6062963627b90cbc1ca9c73732a5d7c992
InvictusSlayer <invictusslayer@gmail.com>
2026-01-02 16:32:07 +0000

    Bump version 1.5.1 Fixed wrong jar files being uploaded Temporarily disabled vanilla compat - closes #22 Removed redundant mixin Added scabbard dependency checking and entrypoint

--- common/src/main/java/net/invictusslayer/slayersbeasts/SlayersBeasts.java
+++ common/src/main/java/net/invictusslayer/slayersbeasts/SlayersBeasts.java
@@ -76,8 +76,8 @@ public class SlayersBeasts {
 		LOGGER.info("Extended Boat$Type values: {}", Arrays.toString(Boat.Type.values()));
 		LOGGER.info("Extended Fox$Type values: {}", Arrays.toString(Fox.Type.values()));
 
-		SBFlammableBlocks.register();
-		SBStrippableBlocks.register();
+//		SBFlammableBlocks.register();
+//		SBStrippableBlocks.register();
 		SBDispensableItems.register();
 		SBVillagerType.setupBiomes();
 
@@ -93,8 +93,8 @@ public class SlayersBeasts {
 
 	public static void clientSetup() {
 		SBWoodType.values().forEach(type -> {
-			Sheets.SIGN_MATERIALS.put(type, Sheets.createSignMaterial(type));
-			Sheets.HANGING_SIGN_MATERIALS.put(type, Sheets.createHangingSignMaterial(type));
+//			Sheets.SIGN_MATERIALS.put(type, Sheets.createSignMaterial(type));
+//			Sheets.HANGING_SIGN_MATERIALS.put(type, Sheets.createHangingSignMaterial(type));
 		});
 	}
 
